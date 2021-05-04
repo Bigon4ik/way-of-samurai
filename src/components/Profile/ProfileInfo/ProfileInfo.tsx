aad121cf48c662e35852ff2684392cb2f97f3249
Bigon4ik <bigon444ik@gmail.com>
2021-05-04 12:42:04 +0300

    add ProfileStatus.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus/ProfileStatus';
 
 const ProfileInfo = (props:any) => {
     if(!props.profile){
@@ -8,13 +9,15 @@ const ProfileInfo = (props:any) => {
     }
     return (
         <div className={s.profileinfo}>
-            <img
-                src="https://s.zagranitsa.com/images/articles/3580/870x486/6ee37702048d1e30ebd2e338b85d7f61.jpg?1463482531"
-                alt=""/>
+            {/*<img*/}
+            {/*    src="https://s.zagranitsa.com/images/articles/3580/870x486/6ee37702048d1e30ebd2e338b85d7f61.jpg?1463482531"*/}
+            {/*    alt=""/>*/}
             <div className={s.discriptionBlock}>
                 Full name: {props.profile.fullName}
                 <img src={props.profile.photos.large}/>
+                <ProfileStatus status={'hello mu friends'}/>
                 Search work : {props.profile.lookingForAJobDescription}
+
 
             </div>
 
